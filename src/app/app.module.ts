cd5a6e17c69a02399af59f8362f186bae210ec69
Roberto <roberto.ht2ml@gmail.com>
2020-06-03 19:32:35 -0300

    primeiro comiit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { Module1Module } from './module1/module1.module';
+import { Module2Module } from './module2/module2.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    Module1Module,
+    Module2Module
+
   ],
   providers: [],
   bootstrap: [AppComponent]
